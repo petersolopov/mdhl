75df66da3495a9184957be6f9394c969a215243d
Peter Solopov <solopov.official@gmail.com>
2020-06-24 09:30:37 +0300

    feat: update example

--- example/script.js
+++ example/script.js
@@ -1,9 +1,7 @@
-import Yace from "https://unpkg.com/yace@0.0.4/dist/esm/index.esm.js";
-import { highlight } from "../mdhl.js";
+import Yace from "https://unpkg.com/yace@0.0.5/dist/esm/index.esm.js";
+import { highlight, defaultRenderers } from "../mdhl.js";
 
 const value = `# h1 Heading
-## h2 Heading
-### h3 Heading
 
 Emphasis
 ========
@@ -11,6 +9,24 @@ Emphasis
 1. **This is** __bold text__
 2. *This is* _italic text_
 
+Code
+====
+
+- Inline \`code\`
+- Block code "fences"
+
+\`\`\`js
+function sum(a, b) {
+  return a + b;
+}
+\`\`\`
+
+- Indented code
+
+    // Some comments
+    line 1 of code
+    line 2 of code
+    line 3 of code
 
 Links
 =====
@@ -22,24 +38,6 @@ Blockquotes
 ===========
 
 > awesome quote
-
-Code
-====
-
-- Inline \`code\`
-- Indented code
-
-    // Some comments
-    line 1 of code
-    line 2 of code
-    line 3 of code
-
-
-- Block code "fences"
-
-\`\`\`
-Sample text here...
-\`\`\`
 
 Markup
 ======
@@ -58,8 +56,20 @@ editor.textarea.focus();
 editor.textarea.spellcheck = false;
 
 const highlighted = document.querySelector(".highlighted");
-highlighted.innerHTML = highlight(value);
+
+const renderers = {
+  ...defaultRenderers,
+  codeInFences: (code, language) => {
+    try {
+      return hljs.highlight(language, code).value;
+    } catch (error) {
+      return defaultRenderers.codeInFences(code, language);
+    }
+  },
+};
+
+highlighted.innerHTML = highlight(value, renderers);
 
 editor.onUpdate((value) => {
-  highlighted.innerHTML = highlight(value);
+  highlighted.innerHTML = highlight(value, renderers);
 });
